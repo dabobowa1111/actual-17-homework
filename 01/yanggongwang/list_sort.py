a1ae1ee0aae4c979565a65e0113a3fce28b0ec5f
yanggongwang <yang@yangdeMacBook-Pro.local>
2017-12-04 11:17:29 +0800

    deleted:    "02/yanggongwang/\345\233\236\346\226\207\346\225\260\345\210\244\346\226\255.py"

--- 01/yanggongwang/list_sort.py
+++ 01/yanggongwang/list_sort.py
@@ -3,12 +3,50 @@
 # 作者：杨公旺
 # 时间：2017-11-27
 
+j = 0
+count=0
+arr = [49,38,65,97,76,13,27,49,89,79,190,789,89]
+for i in range(0,len(arr)-1):
+    for j in range(i+1,len(arr)):
+        if arr[i] < arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            print arr
+        count = count +1
+print count
 
-list = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,444,111,3333,4,5,777,65535,45,5001,33,45,6000]
-for i in range(0,len(list)-1):
-    for j in range(i+1,len(list)):
-        if list[i] < list[j]:
-            temp = list[i]
-	    list[i] = list[j]
-	    list[j] = temp
-print list
+
+if pwd == password:
+   print "登录成功"
+      break
+else:
+print "输入错误"
+
+
+
+
+
+num_list = [8,2,4,5,4,6,36]
+
+while True:
+    my_input = raw_input("请输入add/del/p:")
+    if my_input != "add" or my_input != "del" or my_input != "p" or my_input != "exit":
+        print "您只能输入add、del、p或exit"
+        break
+
+
+    if my_input == "add":
+       my_number = raw_input("请输入您要插入的值：")
+           num_list.append(my_number)
+           print "当前列表是%s"%(num_list)
+    if my_input == "del":
+       if len(num_list)>0
+           num_list.pop()
+           print "当前列表是%s"%(num_list)
+        else:
+           print "列表为空，不允许删除操作"
+    if my_input == "p":
+           print "当前列表是%s"%(num_list)
+    if my_input == "exit":
+        break
